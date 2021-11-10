ed0585e10214ce7bd484b9c4e8fb150e21a044b9
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-11 03:37:49 +0530

    ADDED : Welcome to Indian States Census Analyzer

---
+++ src/main/java/com/bridgelabz/indianstatecensus/IndianStateCensusAnalyzer.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.indianstatecensus;
+
+public class IndianStateCensusAnalyzer {
+    public static void main(String[] args) {
+        // Displaying Welcome message
+        System.out.println("Welcome to Indian States Census Analyzer Program");
+    }
+}
