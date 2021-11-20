55f05b96237894708e99e20f69782cf0a5202ef8
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-21 05:06:24 +0530

    ADDED : Checking if passed file has proper extension or not

--- src/test/java/com/bridgelabz/indianstatecensus/IndianStateCodeTest.java
+++ src/test/java/com/bridgelabz/indianstatecensus/IndianStateCodeTest.java
@@ -6,11 +6,23 @@ import java.util.List;
 
 public class IndianStateCodeTest {
     @Test
-    public void givenReadDataFromIndianCensus_ShouldReturnCountDataPresentInFile()
-    {
+    public void givenReadDataFromIndianCensus_ShouldReturnCountDataPresentInFile() {
         String fileName = "/StateCode.csv";
         IndianStateCodeService codeService = new IndianStateCodeService();
         List<StateCode> stateCensusList = codeService.readIndiaStatCode(fileName);
         Assert.assertEquals(37, stateCensusList.size());
+    }
+
+    @Test
+    public void givenWrongFileExtention_ShouldReturnWrongFileType() {
+        String fileName = "/StateCodeData.txt";
+        IndianStateCodeService censusService = new IndianStateCodeService();
+        try {
+            List<StateCode> StateCodeList = censusService.readIndiaStatCode(fileName);
+            Assert.assertEquals(37, StateCodeList.size());
+        }
+        catch (CustomExceptionService e) {
+            Assert.assertEquals(CustomExceptionService.ExceptionType.WRONG_FILE_TYPE, e.type);
+        }
     }
 }
